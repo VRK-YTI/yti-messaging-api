735d78e51232ee3f09c3e90357f19a48b3e436a5
Antti Tohmo <antti.tohmo@gofore.com>
2019-11-29 13:38:48 +0200

    Removing unused imports.

--- src/main/java/fi.vm.yti.messaging/exception/exceptionmapping/YtiMessagingExceptionMapper.java
+++ src/main/java/fi.vm.yti.messaging/exception/exceptionmapping/YtiMessagingExceptionMapper.java
@@ -4,9 +4,6 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import fi.vm.yti.messaging.exception.YtiMessagingException;
 
 @Provider
